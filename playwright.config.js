70234ed629b09e393f3ffb1ae97db2c8d83ce473
darshana-crest <darshana.parekh@crestinfosystems.com>
2026-05-13 14:43:56 +0530

    add workflow file

--- playwright.config.js
+++ playwright.config.js
@@ -1,37 +1,81 @@
+// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 export default defineConfig({
-  testDir: './tests', // Folder where tests are located
-  timeout: 30 * 1000, // Global timeout for each test
-  expect: {
-    timeout: 5000 // Timeout for expect assertions
-  },
-  fullyParallel: true, // Run tests in parallel
-  reporter: [['html'], ['list'], ['allure-playwright']], // Report formats
+  testDir: './e2e',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    // baseURL: 'https://example.com', // Base URL for page.goto()
-    headless: false, // Run tests without opening browser UI
-    viewport: { width: 1280, height: 720 },
-    screenshot: 'only-on-failure', // Take screenshot on failure
-    video: 'retain-on-failure', // Record video on failure
-    trace: 'retain-on-failure',
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
   },
+
+  /* Configure projects for major browsers */
   projects: [
     {
-      name: 'Chromium',
+      name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
     // {
-    //   name: 'firefox',
-    //   use: { 
-    //       ...devices['Desktop Firefox'],
-    //       // Optional: Increase the context/browser launch timeout specifically for Firefox (in ms)
-    //       // If you don't have a specific launch timeout defined, this may help.
-    //       launchTimeout: 60000, }
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
     // },
     // {
-    //   name: 'WebKit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
+
